20eb4bb61d9fbb37be0f0d079691e5ccff640bbc
Tom Doom <malaclypse17@web.de>
2013-02-03 10:41:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piratenradio::Application.config.secret_token = '7b08a0736ffb1c743ae9c569d4426c6e48a4d9dcfc15cfcba295b9a91a10143f10cea8581cbc9cab5633b68fdce70d670db527ae349439565a7d94ca9461a402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Piratenradio::Application.config.session_store :cookie_store, key: '_piratenradio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Piratenradio::Application.config.session_store :active_record_store
